5561b26649f7a3fca606e0e81181b4aa09689954
elimchayseng <elimchayseng13@gmail.com>
2023-05-14 15:49:22 -0500

    updated callback urls

--- src/script.ts
+++ src/script.ts
@@ -12,7 +12,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://etheroku-spotify-vite.herokuapp.com/callback");
     params.append("scope", "user-read-private user-read-email user-read-recently-played user-top-read");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -46,7 +46,7 @@ export async function getAccessToken(clientId: string, code: string): Promise<st
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://etheroku-spotify-vite.herokuapp.com/callback");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
